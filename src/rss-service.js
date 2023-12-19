2cea85a0ad9b1460131c8f11560ee8b85f9a09f0
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-12-19 21:49:48 +0300

    Upd rss-service.js

--- src/rss-service.js
+++ src/rss-service.js
@@ -11,7 +11,7 @@ const addProxy = (url) => {
   return urlWithProxy.toString();
 };
 
-const updateRSS = (state) => {
+function updateRSS = (state) => {
   const requests = state.feeds.map((feed) => axios.get(addProxy(feed.link))
     .then((response) => {
       const [, posts] = parser(response.data.contents);
@@ -34,7 +34,7 @@ const defineError = (err) => {
   }
   return 'unknowError';
 };
-const loadRSS = (url, state) => {
+function loadRSS = (url, state) => {
   axios.get(addProxy(url))
     .then((responce) => {
       const [feed, posts] = parser(responce.data.contents);
