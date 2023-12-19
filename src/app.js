58085ae1102df03409f8529d58eb7677df82f58a
Arthur <139110457+AruMkh@users.noreply.github.com>
2023-12-19 21:31:31 +0300

    Upd app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import _ from 'lodash';
 import axios from 'axios';
 import getState from './render.js';
 import parser from './parser.js';
+import loadRSS from './rss-service.js';
+import updateRSS from './rss-service.js';
 
 import resources from './locales/index.js';
 import locale from './locales/locale.js';
@@ -12,56 +14,6 @@ import locale from './locales/locale.js';
 const timeout = 10000;
 const lng = 'ru';
 
-const addProxy = (url) => {
-  const urlWithProxy = new URL('/get', 'https://allorigins.hexlet.app');
-  urlWithProxy.searchParams.set('url', url);
-  urlWithProxy.searchParams.set('disableCache', 'true');
-  return urlWithProxy.toString();
-};
-
-const updateRSS = (state) => {
-  const requests = state.feeds.map((feed) => axios.get(addProxy(feed.link))
-    .then((response) => {
-      const [, posts] = parser(response.data.contents);
-      const postsFromState = state.posts.filter((post) => post.feedId === feed.id);
-      const newPosts = _.differenceBy(posts, postsFromState, 'link');
-      state.posts = [...newPosts, ...state.posts];
-    })
-    .catch((err) => console.log(err)));
-  Promise.all(requests)
-    .then(() => {
-      setTimeout(updateRSS, timeout, state);
-    });
-};
-const defineError = (err) => {
-  if (err.isAxiosError) {
-    return 'networkError';
-  }
-  if (err.isParserError) {
-    return 'parserError';
-  }
-  return 'unknowError';
-};
-const loadRSS = (url, state) => {
-  axios.get(addProxy(url))
-    .then((responce) => {
-      const [feed, posts] = parser(responce.data.contents);
-      feed.id = _.uniqueId();
-      feed.link = url;
-      state.feeds.push(feed);
-      posts.forEach((post) => {
-        post.id = _.uniqueId();
-        post.feedId = feed.id;
-      });
-      state.posts = [...posts, ...state.posts];
-      state.status = 'loaded';
-      state.error = null;
-    })
-    .catch((err) => {
-      state.error = defineError(err);
-      state.status = 'failed';
-    });
-};
 export default () => {
   const i18n = i18next.createInstance();
   i18n.init({
